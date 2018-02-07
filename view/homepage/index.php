06bd999581b469eaa760fcc25cbcc6ab13ea9c16
murariwalake <murariwalake@gmail.com>
2018-02-07 16:09:13 +0530

    These changes

--- view/homepage/index.php
+++ view/homepage/index.php
@@ -6,8 +6,13 @@
   </head>
   <body>
     <div class="">
-      <button type="button" ><a href="home/teacher">Teacher</a></button>
-      <button type="button" ><a href="home/student">Student</a></button>
+      <form action="loginred" method="post">
+        <input type="text" name="username"><br>
+        <input type="password" name="passvalue"><br>
+        <input type="checkbox" name="type" value="s">Student</input><br>
+        <input type="checkbox" name="type" value="t">Teacher</input><br>
+        <input type="submit" value="Login">
+      </form>
     </div>
   </body>
 </html>
